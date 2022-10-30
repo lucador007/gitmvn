e52bd239b83bc3c461e0d00cecd7467623aa86bd
lucad <lucad@Kamakshi>
2022-10-30 23:05:42 +0530

    First Code checkin

---
+++ com.sel.mvn/src/test/java/com/sel/mvn/testMvn.java
@@ -0,0 +1,20 @@
+package com.sel.mvn;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class testMvn 
+{
+	@Test
+	public void LoadURL()
+	{
+		//System.setProperty("webdriver.chrome.driver", "C:\\_KIRAN\\Work\\Selenium\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+		//driver.get("https://www.google.com");
+		
+		System.out.println("Hello Maven!");
+	}
+	
+
+}
